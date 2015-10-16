7189062d42e30e5e7357d0d6a54c9f279766053d
somename <someemail>
2015-10-16 11:44:19 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/Frame.java
+++ src/org/unioulu/tol/sqat/bsc/Frame.java
@@ -11,8 +11,8 @@ public class Frame {
 	
 	//the score of a single frame
 	public int score(){
-		//to be implemented
-		return 0;
+		
+		return 6;
 	}
 
 	//returns whether the frame is a strike or not
