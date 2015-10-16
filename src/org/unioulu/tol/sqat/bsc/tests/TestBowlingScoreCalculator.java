3ad5e7b330ee419a68569bcfedac1351ae21a7ee
somename <someemail>
2015-10-16 11:43:43 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -3,12 +3,17 @@ package org.unioulu.tol.sqat.bsc.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat.bsc.Frame;
 
 public class TestBowlingScoreCalculator {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testScorePositiveNumbersPositiveNumbersOut() {
+		
+		Frame frame = new Frame(1,5);
+		
+		assertEquals(6, frame.score());
+		
 	}
 
 }
