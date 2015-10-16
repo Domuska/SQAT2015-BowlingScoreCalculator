ca3f49b55a1e5d9bcbf813d272fac84d95bdf5e1
somename <someemail>
2015-10-16 12:11:24 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -8,7 +8,7 @@ import org.unioulu.tol.sqat.bsc.Frame;
 public class TestBowlingScoreCalculator {
 
 	@Test
-	public void testScoreOneAndFiveSixOut() {
+	public void testFrameScoreOneAndFiveSixOut() {
 		
 		Frame frame = new Frame(1,5);
 		
@@ -17,7 +17,7 @@ public class TestBowlingScoreCalculator {
 	}
 	
 	@Test
-	public void testScoreSixAndFourTenOut(){
+	public void testFrameScoreSixAndFourTenOut(){
 		
 		Frame frame = new Frame(6,4);
 		
@@ -25,7 +25,7 @@ public class TestBowlingScoreCalculator {
 	}
 	
 	@Test
-	public void testScoreMinusFiveAndOneErrorNumberOut(){
+	public void testFrameScoreMinusFiveAndOneErrorNumberOut(){
 		
 		Frame frame = new Frame(-5,1);
 		
@@ -33,7 +33,7 @@ public class TestBowlingScoreCalculator {
 	}
 
 	@Test
-	public void testSpareIsNotSpare(){
+	public void testFrameSpareIsNotSpare(){
 		
 		Frame frame = new Frame(5,2);
 		
@@ -41,7 +41,7 @@ public class TestBowlingScoreCalculator {
 	}
 	
 	@Test
-	public void testSpareIsSpare(){
+	public void testFrameSpareIsSpare(){
 		
 		Frame frame = new Frame(4,6);
 		
@@ -49,7 +49,7 @@ public class TestBowlingScoreCalculator {
 	}
 	
 	@Test
-	public void testBonusAdditionPlusFive(){
+	public void testFrameBonusAdditionPlusFive(){
 		
 		Frame frame = new Frame(4,6);
 		
@@ -60,7 +60,7 @@ public class TestBowlingScoreCalculator {
 	}
 	
 	@Test
-	public void testBonusAdditionMinusFiveShouldFail(){
+	public void testFrameBonusAdditionMinusFiveShouldFail(){
 		
 		Frame frame = new Frame(4,6);
 		frame.addBonus(-5);
@@ -69,7 +69,7 @@ public class TestBowlingScoreCalculator {
 	}
 	
 	@Test
-	public void testBonusAdditionPlusTenPlusFifteenFiveShouldFail(){
+	public void testFrameBonusAdditionPlusTenPlusFifteenFiveShouldFail(){
 		
 		Frame frame = new Frame(4,6);
 		frame.addBonus(15);
@@ -78,5 +78,7 @@ public class TestBowlingScoreCalculator {
 		assertEquals(20, frame.bonus());
 		
 	}
+	
+	
 	
 }
